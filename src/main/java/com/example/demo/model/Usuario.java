8b77b4dd78256432a09577e7e79abfd0f3f235ff
andresortegac <andres.ortegac97@gmail.com>
2023-05-23 22:33:05 -0500

    Correccion del GenerationType del modelo Usuario

--- src/main/java/com/example/demo/model/Usuario.java
+++ src/main/java/com/example/demo/model/Usuario.java
@@ -13,7 +13,7 @@ import jakarta.persistence.Table;
 public class Usuario {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	
 	@Column(name = "id")
 	private int id;
