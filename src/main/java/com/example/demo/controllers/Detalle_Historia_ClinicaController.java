d38db76024e570bf794ef08cfcdbba190d59e53c
andresortegac <andres.ortegac97@gmail.com>
2023-05-25 22:31:29 -0500

    Anexo de la anotacion CrossOrigin en el controller

--- src/main/java/com/example/demo/controllers/Detalle_Historia_ClinicaController.java
+++ src/main/java/com/example/demo/controllers/Detalle_Historia_ClinicaController.java
@@ -7,6 +7,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,6 +22,7 @@ import com.example.demo.service.Detalle_Historia_ClinicaService;
 
 @RestController
 @RequestMapping("/detalle_historia_clinica/")
+@CrossOrigin({"*"})
 public class Detalle_Historia_ClinicaController {
 
 	@Autowired
